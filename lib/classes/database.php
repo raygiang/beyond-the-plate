8459d7eba2d494b6d5d01350faddfd68ed4da5f1
BirinderSinghN01321133 <erbirindersingh@gmail.com>
2019-03-22 15:07:45 -0400

    Update By Birinder

--- lib/classes/database.php
+++ lib/classes/database.php
@@ -1,36 +1,35 @@
 <?php
-	
-	class Database
-	{
-		private static $user = 'root';
-		private static $pass = '';
-		private static $db = 'cookbook';
-		private static $dsn = 'mysql:host=localhost;dbname=cookbook';
-		private static $dbcon;
 
-		private function __construct()
-		{
+class Database{
 
-		}
+	private static $user = 'root';
+	private static $pass = '';
+	private static $dsn = 'mysql:host=localhost;dbname=cookbook';
+	private static $dbcon;
 
-		public static function getDb()
-		{
-			if(!isset(self::$dbcon))
-			{
-				try
-				{
-					self::$dbcon=new PDO(self::$dsn, self::$user, self::$pass);
-					self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				} 
-				catch(PDOException $e)
-				{
-					$msg = $e->getMessage();
-					include 'customeror.php';
-					exit();
-				}
-			}
-			return self::$dbcon;
-		}	
+	private function __construct(){
+
 	}
-	
+//get pdo connection
+	public static function getDb(){
+		if(!isset(self::$dbcon))
+		try{
+			self::$dbcon = new PDO(self::$dsn, self::$user, self::$pass);
+
+		//show the sql error
+			self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}catch
+			(PDOException $e){
+				echo $msg = $e->getMessage();
+				// include 'customerror.php';
+				exit();
+			}
+
+
+		return self::$dbcon;
+	}
+}
+
+
+
 ?>
