f4544e4c6e609debac4e98dab76830f319516589
Mariya An <mariya7@gmail.com>
2019-03-22 15:58:05 -0400

    Revert "Update By Birinder"
    This reverts commit 8459d7eba2d494b6d5d01350faddfd68ed4da5f1.

--- lib/classes/database.php
+++ lib/classes/database.php
@@ -1,35 +1,36 @@
 <?php
+	
+	class Database
+	{
+		private static $user = 'root';
+		private static $pass = '';
+		private static $db = 'cookbook';
+		private static $dsn = 'mysql:host=localhost;dbname=cookbook';
+		private static $dbcon;
 
-class Database{
+		private function __construct()
+		{
 
-	private static $user = 'root';
-	private static $pass = '';
-	private static $dsn = 'mysql:host=localhost;dbname=cookbook';
-	private static $dbcon;
+		}
 
-	private function __construct(){
-
-	}
-//get pdo connection
-	public static function getDb(){
-		if(!isset(self::$dbcon))
-		try{
-			self::$dbcon = new PDO(self::$dsn, self::$user, self::$pass);
-
-		//show the sql error
-			self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}catch
-			(PDOException $e){
-				echo $msg = $e->getMessage();
-				// include 'customerror.php';
-				exit();
+		public static function getDb()
+		{
+			if(!isset(self::$dbcon))
+			{
+				try
+				{
+					self::$dbcon=new PDO(self::$dsn, self::$user, self::$pass);
+					self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				} 
+				catch(PDOException $e)
+				{
+					$msg = $e->getMessage();
+					include 'customeror.php';
+					exit();
+				}
 			}
-
-
-		return self::$dbcon;
+			return self::$dbcon;
+		}	
 	}
-}
-
-
-
+	
 ?>
